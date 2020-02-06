15f97879c517b1aedcadca1f85f7d5b287a7ed9d
Shahin <shahinshahrokh@rocketmail.com>
2020-02-06 17:23:21 +0000

    password_validation

--- Application/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/navigation/common/R.java
+++ Application/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/navigation/common/R.java
@@ -98,26 +98,26 @@ public final class R {
         public static final int async = 0x7f090041;
         public static final int blocking = 0x7f090046;
         public static final int chronometer = 0x7f090052;
-        public static final int forever = 0x7f09007b;
-        public static final int icon = 0x7f090084;
-        public static final int icon_group = 0x7f090085;
-        public static final int info = 0x7f09008c;
-        public static final int italic = 0x7f09008f;
-        public static final int line1 = 0x7f090095;
-        public static final int line3 = 0x7f090096;
-        public static final int normal = 0x7f0900a6;
-        public static final int notification_background = 0x7f0900a7;
-        public static final int notification_main_column = 0x7f0900a8;
-        public static final int notification_main_column_container = 0x7f0900a9;
-        public static final int right_icon = 0x7f0900bb;
-        public static final int right_side = 0x7f0900bc;
-        public static final int tag_transition_group = 0x7f0900f1;
-        public static final int tag_unhandled_key_event_manager = 0x7f0900f2;
-        public static final int tag_unhandled_key_listeners = 0x7f0900f3;
-        public static final int text = 0x7f0900f4;
-        public static final int text2 = 0x7f0900f5;
-        public static final int time = 0x7f0900fd;
-        public static final int title = 0x7f0900fe;
+        public static final int forever = 0x7f09007c;
+        public static final int icon = 0x7f090085;
+        public static final int icon_group = 0x7f090086;
+        public static final int info = 0x7f09008d;
+        public static final int italic = 0x7f090090;
+        public static final int line1 = 0x7f090096;
+        public static final int line3 = 0x7f090097;
+        public static final int normal = 0x7f0900a7;
+        public static final int notification_background = 0x7f0900a8;
+        public static final int notification_main_column = 0x7f0900a9;
+        public static final int notification_main_column_container = 0x7f0900aa;
+        public static final int right_icon = 0x7f0900bc;
+        public static final int right_side = 0x7f0900bd;
+        public static final int tag_transition_group = 0x7f0900f2;
+        public static final int tag_unhandled_key_event_manager = 0x7f0900f3;
+        public static final int tag_unhandled_key_listeners = 0x7f0900f4;
+        public static final int text = 0x7f0900f5;
+        public static final int text2 = 0x7f0900f6;
+        public static final int time = 0x7f0900fe;
+        public static final int title = 0x7f0900ff;
     }
     public static final class integer {
         private integer() {}
