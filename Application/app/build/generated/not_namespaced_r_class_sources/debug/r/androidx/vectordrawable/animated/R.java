345a8ff87957c589cb79b69d8c74f660fb26e086
Shahin <shahinshahrokh@rocketmail.com>
2020-02-12 11:13:14 +0000

    checks_admin

--- Application/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/vectordrawable/animated/R.java
+++ Application/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/vectordrawable/animated/R.java
@@ -133,18 +133,18 @@ public final class R {
         public static final int notification_main_column_container = 0x7f0900ae;
         public static final int right_icon = 0x7f0900c0;
         public static final int right_side = 0x7f0900c1;
-        public static final int tag_accessibility_actions = 0x7f0900f2;
-        public static final int tag_accessibility_clickable_spans = 0x7f0900f3;
-        public static final int tag_accessibility_heading = 0x7f0900f4;
-        public static final int tag_accessibility_pane_title = 0x7f0900f5;
-        public static final int tag_screen_reader_focusable = 0x7f0900f6;
-        public static final int tag_transition_group = 0x7f0900f7;
-        public static final int tag_unhandled_key_event_manager = 0x7f0900f8;
-        public static final int tag_unhandled_key_listeners = 0x7f0900f9;
-        public static final int text = 0x7f0900fa;
-        public static final int text2 = 0x7f0900fb;
-        public static final int time = 0x7f090103;
-        public static final int title = 0x7f090104;
+        public static final int tag_accessibility_actions = 0x7f0900f1;
+        public static final int tag_accessibility_clickable_spans = 0x7f0900f2;
+        public static final int tag_accessibility_heading = 0x7f0900f3;
+        public static final int tag_accessibility_pane_title = 0x7f0900f4;
+        public static final int tag_screen_reader_focusable = 0x7f0900f5;
+        public static final int tag_transition_group = 0x7f0900f6;
+        public static final int tag_unhandled_key_event_manager = 0x7f0900f7;
+        public static final int tag_unhandled_key_listeners = 0x7f0900f8;
+        public static final int text = 0x7f0900f9;
+        public static final int text2 = 0x7f0900fa;
+        public static final int time = 0x7f090102;
+        public static final int title = 0x7f090103;
     }
     public static final class integer {
         private integer() {}
