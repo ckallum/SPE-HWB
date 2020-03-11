b23125fc9d1eaaa6b28b139017abd9a5ac389964
Omar <omarwsuleiman@gmail.com>
2020-03-11 17:49:50 +0000

    venues started

--- Application/app/src/main/java/com/example/healthproject/Fragments/VenuesFragment.java
+++ Application/app/src/main/java/com/example/healthproject/Fragments/VenuesFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.view.View.*;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +16,44 @@ import com.example.healthproject.R;
 
 
 public class VenuesFragment extends Fragment {
+    ImageView indoor;
+    ImageView coombe;
+    ImageView su;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_venues,container,false);
+        final View rootView = inflater.inflate(R.layout.fragment_venues,container,false);
+
+        indoor = rootView.findViewById(R.id.indoor);
+        coombe = rootView.findViewById(R.id.coombe);
+        su = rootView.findViewById(R.id.richmond);
+
+        indoor.setClickable(true);
+        coombe.setClickable(true);
+        su.setClickable(true);
+
+        indoor.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(rootView.getContext(), "indoor", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        coombe.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(rootView.getContext(), "coombe", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        su.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(rootView.getContext(), "su", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
     }
 }
