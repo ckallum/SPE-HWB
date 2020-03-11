d6b678ac235a53f0a535ceba6c4c161aebde9bf1
Omar <omarwsuleiman@gmail.com>
2020-03-11 21:47:03 +0000

    .

--- Application/app/src/main/java/com/example/healthproject/Fragments/IndoorFragment.java
+++ Application/app/src/main/java/com/example/healthproject/Fragments/IndoorFragment.java
@@ -1,29 +1,39 @@
 package com.example.healthproject.Fragments;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.healthproject.Activity.MainActivity;
 import com.example.healthproject.R;
 
 public class IndoorFragment extends Fragment {
     ImageView backArrow;
+    ImageView map;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_indoor,container,false);
+        final View rootView = inflater.inflate(R.layout.fragment_indoor,container,false);
 
         backArrow = rootView.findViewById(R.id.back);
+        map = rootView.findViewById(R.id.tyndallmap);
 
         backArrow.setClickable(true);
+        map.setClickable(true);
 
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +46,37 @@ public class IndoorFragment extends Fragment {
             }
         });
 
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openApp(getActivity(), "com.google.android.gms.maps");
+            }
+        });
+
         return rootView;
 
+    }
+
+    /** Open another app.
+     * @param context current Context, like Activity, App, or Service
+     * @param packageName the full package name of the app to open
+     * @return true if likely successful, false if unsuccessful
+     */
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        try {
+            Intent i = manager.getLaunchIntentForPackage(packageName);
+            if (i == null) {
+//                Toast.makeText(activity, "if null", Toast.LENGTH_SHORT).show();
+                return false;
+                //throw new ActivityNotFoundException();
+            }
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            context.startActivity(i);
+            return true;
+        } catch (ActivityNotFoundException e) {
+//            Toast.makeText(activity, "not found exception", Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
--- Application/app/src/main/java/com/example/healthproject/Fragments/VenuesFragment.java
+++ Application/app/src/main/java/com/example/healthproject/Fragments/VenuesFragment.java
@@ -63,4 +63,6 @@ public class VenuesFragment extends Fragment {
 
         return rootView;
     }
+
+
 }
