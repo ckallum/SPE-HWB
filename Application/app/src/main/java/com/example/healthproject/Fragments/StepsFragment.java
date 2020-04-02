790ae00365acb8e8bbb9220d3705c61640a39392
shashabinks <shahinshahrokh@rocketmail.com>
2020-04-02 15:32:22 +0100

    step tracker inaccurate

--- Application/app/src/main/java/com/example/healthproject/Fragments/StepsFragment.java
+++ Application/app/src/main/java/com/example/healthproject/Fragments/StepsFragment.java
@@ -1,6 +1,7 @@
 package com.example.healthproject.Fragments;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -27,13 +28,17 @@ import com.example.healthproject.R;
 
 import java.util.ArrayList;
 
+import static android.view.View.getDefaultSize;
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
-public class StepsFragment extends Fragment implements SensorEventListener {
+public class StepsFragment extends Fragment{
 
 SensorManager sensorManager;
 Context context;
-TextView steps;
+private TextView steps;
+private double previousMag = 0;
+private Integer stepCount = 0;
+
 Boolean running = false;
 
     @Nullable
@@ -41,52 +46,76 @@ Boolean running = false;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
        final View v =inflater.inflate(R.layout.steps_fragment, container, false);
+
+        steps = v.findViewById(R.id.tv_steps);
+        sensorManager = (SensorManager) getContext().getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if(event != null){
+                    float x_acc = event.values[0];  //acceleration
+                    float y_acc = event.values[1];
+                    float z_acc = event.values[2];
+
+                    double magnitude = Math.sqrt(x_acc*x_acc + y_acc*y_acc + z_acc* z_acc);
+                    double magDelta = magnitude - previousMag;
+                    previousMag = magnitude;
+
+                    if(magDelta > 6){
+                        stepCount++;
+                    }
+                    steps.setText(stepCount.toString());
+
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sensorManager.registerListener(stepDetector,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+
         return v;
 
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        steps = getActivity().findViewById(R.id.tv_steps);
-        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        running = true;
-        Sensor countS = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-
-        if(countS != null){
-            sensorManager.registerListener(this, countS,SensorManager.SENSOR_DELAY_UI);
-
-        }else{
-            Toast.makeText(getContext(),"Sensor not found",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
     public void onPause() {
+
         super.onPause();
-        running = false;
-        //sensorManager.unregisterListener(this);
+
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor     =  sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepcount",stepCount);
+        editor.apply();
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onStop() {
 
-        if(running){
-            steps.setText(String.valueOf(event.values[0]));
-        }
+        super.onStop();
+
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor     =  sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepcount",stepCount);
+        editor.apply();
+    }
+
+    public void onResume() {  //set value when resume app, default 0. Maybe implement an alarm?
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        stepCount = sharedPreferences.getInt("stepCount",0);
 
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
+
 }
 
 
