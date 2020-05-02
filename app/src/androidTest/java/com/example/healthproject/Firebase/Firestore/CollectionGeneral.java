f924b975b63a1d1321b8d72c528caba92196488b
Callum Ke <in18536@bristol.ac.uk>
2020-05-02 19:04:09 +0100

    Done for the day

--- app/src/androidTest/java/com/example/healthproject/Firebase/Firestore/CollectionGeneral.java
+++ app/src/androidTest/java/com/example/healthproject/Firebase/Firestore/CollectionGeneral.java
@@ -2,18 +2,34 @@ package com.example.healthproject.Firebase.Firestore;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.junit.Rule;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class CollectionGeneral {
     //Check if collections exist
-    @Rule
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+    @Before
+    public void setUp(){
+    }
 
     private ArrayList<String> collectionNames = new ArrayList<>(Arrays.asList("events", "users", "venues", "user_event_links"));
 
+    @Test
+    public void testCollectionsExist(){
+        for( int i = 0; i < collectionNames.size(); i++ ){
+            db.collection(collectionNames.get(i)).get().addOnSuccessListener(queryDocumentSnapshots -> {
+                assertTrue(queryDocumentSnapshots.size() > 0);
+            } );
+        }
+    }
 
 }
 
