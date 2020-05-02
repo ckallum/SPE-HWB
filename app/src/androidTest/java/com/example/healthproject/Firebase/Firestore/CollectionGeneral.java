d7d9d26b8b1194369f5b2270870e61cfab7ce842
Callum Ke <in18536@bristol.ac.uk>
2020-05-02 18:52:49 +0100

    Fixed some tests

--- app/src/androidTest/java/com/example/healthproject/Firebase/Firestore/CollectionGeneral.java
+++ app/src/androidTest/java/com/example/healthproject/Firebase/Firestore/CollectionGeneral.java
@@ -1,5 +1,19 @@
 package com.example.healthproject.Firebase.Firestore;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Rule;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CollectionGeneral {
     //Check if collections exist
+    @Rule
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private ArrayList<String> collectionNames = new ArrayList<>(Arrays.asList("events", "users", "venues", "user_event_links"));
+
+
 }
+
